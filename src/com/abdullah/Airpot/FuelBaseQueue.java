493cb391519cafb9907f97adf49511ede3d54ee7
Abdullah bhatti <bhatti253r@gmail.com>
2024-10-06 18:59:29 +0500

    feat: Airport System implements queue data structure

---
+++ src/com/abdullah/Airpot/FuelBaseQueue.java
@@ -0,0 +1,53 @@
+package com.abdullah.Airpot;
+
+import java.util.ArrayList;
+
+public class FuelBaseQueue {
+    ArrayList<Plane> planes = new ArrayList<>();
+    public void addForLanding(int planeId, int priority) {
+         planes.add(new Plane(planeId, priority));
+    }
+    public Plane removeForLanding() {
+        int minFuelLevel = Integer.MAX_VALUE;
+        for (Plane plane : planes) {
+            if (plane.fuelLevel < minFuelLevel) {
+                minFuelLevel = plane.fuelLevel;
+            }
+        }
+        for (Plane plane : planes) {
+            if (plane.fuelLevel == minFuelLevel) {
+                planes.remove(plane);
+                return plane;
+            }
+        }
+        return null;
+    }
+    public void displayLandingPlanes(){
+        for (Plane plane : planes) {
+            System.out.println(plane);
+        }
+    }
+
+
+
+
+    private class Plane{
+        int planeId;
+        int fuelLevel;
+
+        public Plane() {
+        }
+
+        public Plane(int planeId, int fuelLevel) {
+            this.planeId = planeId;
+            this.fuelLevel = fuelLevel;
+        }
+
+        @Override
+        public String toString() {
+            return
+                    "Plane ID:  L" + planeId +
+                    ", Fuel Level: " + fuelLevel;
+        }
+    }
+}
