c0d32d7e1fb93d5077d24d7f79a224746b9bbbe3
Abdullah bhatti <bhatti253r@gmail.com>
2024-10-08 18:59:19 +0500

    docs: implement doubly linked list with deletion methods

---
+++ src/com/abdullah/LinkedList/DLLMain.java
@@ -0,0 +1,17 @@
+package com.abdullah.LinkedList;
+
+public class DLLMain {
+    public static void main(String[] args) {
+        DLL dll = new DLL();
+        dll.insertFirst(5);
+        dll.insertFirst(4);
+        dll.insertFirst(3);
+        dll.insertFirst(2);
+        dll.insertFirst(1);
+        dll.insertFirst(0);
+        dll.display();
+//        dll.displayRev();
+        dll.insertAt(2, 0);
+        dll.display();
+    }
+}
